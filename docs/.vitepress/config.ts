edacda9044914b28b6f1c7afd4e0a281964cdd28
Ian Torres <iantorres@outlook.com>
2026-04-25 01:11:05 -0400

    Add minimal VitePress documentation scaffold

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'Land',
+  description: 'Land framework documentation',
+  themeConfig: {
+    nav: [{ text: 'Get Started', link: '/get-started/' }],
+    sidebar: [
+      {
+        text: 'Get Started',
+        items: [{ text: 'Get Started', link: '/get-started/' }],
+      },
+    ],
+  },
+})
